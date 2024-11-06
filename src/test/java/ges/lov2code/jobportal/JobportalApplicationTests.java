92554a6cd28bcfb2d2f72be1f79ef72eff2461d6
elisha1995 <elishasamuel01@gmail.com>
2024-11-06 10:36:10 +0000

    Create database entities Users and UsersType

---
+++ src/test/java/ges/lov2code/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package ges.lov2code.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
