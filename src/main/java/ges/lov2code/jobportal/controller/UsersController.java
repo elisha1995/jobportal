1e2a70359e1834251f2299dcae2c53639d26bbf2
elisha1995 <elishasamuel01@gmail.com>
2024-11-06 14:02:00 +0000

    Develop registration repositories and controller

--- src/main/java/ges/lov2code/jobportal/controller/UsersController.java
+++ src/main/java/ges/lov2code/jobportal/controller/UsersController.java
@@ -2,11 +2,15 @@ package ges.lov2code.jobportal.controller;
 
 import ges.lov2code.jobportal.entity.Users;
 import ges.lov2code.jobportal.entity.UsersType;
+import ges.lov2code.jobportal.services.UsersService;
 import ges.lov2code.jobportal.services.UsersTypeService;
+import jakarta.validation.Valid;
+import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -14,10 +18,12 @@ import java.util.List;
 public class UsersController {
 
     private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
 
     @Autowired
-    public UsersController(UsersTypeService usersTypeService) {
+    public UsersController(UsersTypeService usersTypeService, UsersService usersService) {
         this.usersTypeService = usersTypeService;
+        this.usersService = usersService;
     }
 
     @GetMapping("/register")
@@ -26,5 +32,12 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users) {
+        //System.out.println("User: " + users);
+        usersService.addNew(users);
+        return "dashboard";
     }
 }
