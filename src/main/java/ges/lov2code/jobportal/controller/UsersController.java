a049ca87f390d46091dde4eb8e7d4d53ce8c82b5
elisha1995 <elishasamuel01@gmail.com>
2024-11-07 10:53:47 +0000

    Create Recruiter Profile - Controller and Service

--- src/main/java/ges/lov2code/jobportal/controller/UsersController.java
+++ src/main/java/ges/lov2code/jobportal/controller/UsersController.java
@@ -4,8 +4,13 @@ import ges.lov2code.jobportal.entity.Users;
 import ges.lov2code.jobportal.entity.UsersType;
 import ges.lov2code.jobportal.services.UsersService;
 import ges.lov2code.jobportal.services.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,6 +50,23 @@ public class UsersController {
             return "register";
         }
         usersService.addNew(users);
-        return "dashboard";
+        return "redirect:/dashboard/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/ges/lov2code/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,23 @@
+package ges.lov2code.jobportal.services;
+
+import ges.lov2code.jobportal.entity.RecruiterProfile;
+import ges.lov2code.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterProfileRepository.findById(id);
+    }
+}
